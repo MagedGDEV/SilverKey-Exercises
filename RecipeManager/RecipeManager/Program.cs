44868a42fab9c8a77245346601812e7eca0047a3
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-04 10:23:11 +0200

    create recipe class and test it
    This class used for recipes data and update and delete recipe information

--- RecipeManager/RecipeManager/Program.cs
+++ RecipeManager/RecipeManager/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var recipie = new Recipie("eggs", new List<string>() { "hot", "old" }, new List<string>() { "cold"}, new List<string>() { "old"});
+recipie.printRecipe();
+recipie.DeleteData("hot", "Ingredients");
+recipie.printRecipe();
+recipie.AddData("hot", "Ingredients");
+recipie.printRecipe();
---
+++ RecipeManager/RecipeManager/Recipie.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+
+public class Recipie
+{
+	public string Title { get; set; }
+	public List<string> Ingredients { get; set; }
+	public List<string> Instructions { get; set; }
+	public List<string> Categories { get; set; }
+
+	public Recipie(string title, List<string> ingredients, List<string> instructions, List<string> categories)
+	{
+		Title = title;
+		Ingredients = ingredients;
+		Instructions = instructions;
+		Categories = categories;
+	}
+
+	public void DeleteData(string item, string listName)
+	{
+		listName = listName.ToUpper();
+		if (listName == "INGREDIENTS")
+		{
+			if (Ingredients.Contains(item))
+			{
+				Ingredients.Remove(item);
+			}
+			else
+			{
+				// report item not in list 
+			}
+		}
+		else if (listName == "INSTRUCTIONS")
+		{
+			if (Instructions.Contains(item))
+			{
+				Instructions.Remove(item);
+			}
+			else
+			{
+				// report item not in list 
+			}
+		}
+		else if (listName == "CATEGORIES")
+		{
+			if (Categories.Contains(item))
+			{
+				Categories.Remove(item);
+			}
+			else
+			{
+				// report item not in list 
+			}
+		}
+		else
+		{
+			// report invalid list 
+		}
+	}
+
+	public void AddData(string item, string listName)
+	{
+		listName = listName.ToUpper();
+		if (listName == "INGREDIENTS")
+		{
+			if (Ingredients.Contains(item))
+			{
+				// report item already available
+			}
+			else
+			{
+				Ingredients.Add(item);
+			}
+		}
+		else if (listName == "INSTRUCTIONS")
+		{
+			if (Instructions.Contains(item))
+			{
+				// report item already available
+			}
+			else
+			{
+				Instructions.Add(item);
+			}
+		}
+		else if (listName == "CATEGORIES")
+		{
+			if (Categories.Contains(item))
+			{
+				// report item already available
+			}
+			else
+			{
+				Categories.Add(item);
+			}
+		}
+		else
+		{
+			// report invalid list 
+		}
+	}
+
+	public void printRecipe()
+	{
+		Console.WriteLine("Recipe " + Title);
+		Console.WriteLine("Ingredients");
+		foreach (string ingredient in Ingredients)
+		{
+			Console.Write(ingredient + " ");
+		}
+		Console.WriteLine("\nInstructions");
+		foreach (string instruction in Instructions)
+		{
+			Console.Write(instruction + " ");
+		}
+		Console.WriteLine("\nCategories");
+		foreach (string Category in Categories)
+		{
+			Console.Write(Category + " ");
+		}
+        Console.WriteLine("\n------------");
+	}
+}
+
