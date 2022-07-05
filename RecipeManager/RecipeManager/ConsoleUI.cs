6b87f5b63b510f2fd012df59f0af8ce702bbc909
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-05 13:59:43 +0200

    finish UI welcome message

--- RecipeManager/RecipeManager/ConsoleUI.cs
+++ RecipeManager/RecipeManager/ConsoleUI.cs
@@ -5,18 +5,29 @@ public class ConsoleUI
 	public ConsoleUI()
 	{
 		ConsoleTitle();
+		WelcomeMessage();
+		WelcomeChoices();
 	}
 
 	public void ConsoleTitle()
-    {
-		
-		AnsiConsole.Write(
-		new Panel(
-		new FigletText("Recipe Manager")
-	   .Centered()
-	   
-	   .Color(Color.Blue))
-		.HeavyBorder()
-		.RoundedBorder());
+	{
+		var titleText = new FigletText("Recipe Manager").Centered().Color(Color.Blue);
+		var roundedPanel = new Panel(titleText).DoubleBorder();
+		AnsiConsole.Write(roundedPanel);
+
+	}
+
+	public void WelcomeMessage()
+	{
+		AnsiConsole.Write("\n");
+		var welcomeText = new Markup("[bold yellow]Welcome to Recipe Manager [/]").Centered();
+		AnsiConsole.Write(welcomeText);
+		AnsiConsole.Write("\n");
+	}
+
+	public void WelcomeChoices()
+	{
+		var header = new Markup("What would you are looking for today ?");
+		AnsiConsole.Write(header);
 	}
 }
