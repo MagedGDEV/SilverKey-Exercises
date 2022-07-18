9271fca1c7a1c0b66edfed2940b19061c01b0538
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-18 17:01:41 +0200

    hello world application

--- RecipeManagerWithAPI/BackEnd/Program.cs
+++ RecipeManagerWithAPI/BackEnd/Program.cs
@@ -1,20 +1,6 @@
-﻿using System.Text.Json;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-
-app.MapGet("/", () => Results.Json(new Todo
-{
-    Name = "Walk dog",
-    IsComplete = false
-}, options));
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
-class Todo
-{
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-}
