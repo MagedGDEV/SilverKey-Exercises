107d55bcb5b14ee023bc05dcbfbebbdeb94caf45
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-18 08:33:09 +0200

    finish the tutorial to minimal api by Microsoft

--- RecipeManagerWithAPI/BackEnd/Program.cs
+++ RecipeManagerWithAPI/BackEnd/Program.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
+﻿using System.Text.Json;
 
-WebApplication app = WebApplication.Create();
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-app.Run(async context =>
+var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+app.MapGet("/", () => Results.Json(new Todo
 {
-    // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
-    await context.Response.WriteAsync("hellooo.\n");
-    await context.Response.WriteAsync("Hello world .NET 6. Make sure you run this app using 'dotnet watch run'.");
-});
+    Name = "Walk dog",
+    IsComplete = false
+}, options));
 
-await app.RunAsync();
+app.Run();
+
+class Todo
+{
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
