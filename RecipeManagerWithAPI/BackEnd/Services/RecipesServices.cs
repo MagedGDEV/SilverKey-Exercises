4595d28aa6ec450aa17b7f4fb3e19a866fd17d3d
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-19 14:32:12 +0200

    Finish Editing Ingredients and some from editing instructions

--- RecipeManagerWithAPI/BackEnd/Services/RecipesServices.cs
+++ RecipeManagerWithAPI/BackEnd/Services/RecipesServices.cs
@@ -47,12 +47,61 @@ public class RecipesServices
         return Results.Json(_recipes[recipeId], statusCode: 200);
     }
 
+    private IResult EditRecipeTitle (Guid recipeId, [FromBody] string newTitle)
+    {
+        _recipes[recipeId].EditTitle(newTitle);
+        WriteRecipes();
+        return Results.Json(newTitle, statusCode: 200);
+    }
+
+    private IResult DeleteRecipeIngredients (Guid recipeId, [FromBody] List<string> ingredientsToDelete)
+    {
+        _recipes[recipeId].DeleteIngredients(ingredientsToDelete);
+        WriteRecipes();
+        return Results.Json(ingredientsToDelete, statusCode: 200);
+    }
+
+    private IResult AddRecipeIngredients (Guid recipeId, [FromBody] AddListModel ingredientToAdd)
+    {
+        _recipes[recipeId].AddIngredient(ingredientToAdd.ItemToAdd, ingredientToAdd.Position);
+        WriteRecipes();
+        return Results.Json(ingredientToAdd, statusCode: 200);
+    }
+
+    private IResult EditRecipeIngredients (Guid recipeId, string ingredient, [FromBody] string updatedIngredient)
+    {
+        _recipes[recipeId].EditIngredient(ingredient, updatedIngredient);
+        WriteRecipes();
+        return Results.Json(updatedIngredient, statusCode: 200);
+    }
+
+    private IResult DeleteRecipeInstructions(Guid recipeId, [FromBody] List<string> instructionsToDelete)
+    {
+        _recipes[recipeId].DeleteInstructions(instructionsToDelete);
+        WriteRecipes();
+        return Results.Json(instructionsToDelete, statusCode: 200);
+    }
+
+    private IResult AddRecipeInstructions(Guid recipeId, [FromBody] AddListModel instructionToAdd)
+    {
+        _recipes[recipeId].AddInstruction(instructionToAdd.ItemToAdd, instructionToAdd.Position);
+        WriteRecipes();
+        return Results.Json(instructionToAdd, statusCode: 200);
+    }
+
     public void Routing(IEndpointRouteBuilder router)
     {
         router.MapGet("/recipes", ReadRecipes);
         router.MapPost("/recipes", AddRecipe);
         router.MapGet("/recipes/{recipeId}", GetRecipe);
         router.MapDelete("/recipes/{recipeId}", DeleteRecipe);
+        router.MapPut("/recipe/{recipeId}/title", EditRecipeTitle);
+        router.MapDelete("/recipe/{recipeId}/ingredients", DeleteRecipeIngredients);
+        router.MapPost("/recipe/{recipeId}/ingredients", AddRecipeIngredients);
+        router.MapPut("/recipe/{recipeId}/{ingredient}", EditRecipeIngredients);
+        router.MapDelete("/recipe/{recipeId}/instructions", DeleteRecipeInstructions);
+        router.MapPost("/recipe/{recipeId}/instructions", AddRecipeInstructions);
+
     }
 }
 
