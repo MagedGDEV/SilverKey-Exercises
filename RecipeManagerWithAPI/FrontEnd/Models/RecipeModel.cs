93c96075b5f3a9ab2d839210c06020071792aaca
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-19 03:19:01 +0200

    Add recipeModel

--- RecipeManagerWithAPI/FrontEnd/Models/RecipeModel.cs
+++ RecipeManagerWithAPI/FrontEnd/Models/RecipeModel.cs
@@ -1,9 +1,15 @@
-﻿using System;
-public class RecipeModel
+﻿public class RecipeModel
 {
-    public RecipeModel()
+    public string Title { get; set; }
+    public List<string> Ingredients { get; set; }
+    public List<string> Instructions { get; set; }
+    public List<string> Categories { get; set; }
+    public RecipeModel(string title, List<string> ingredients, List<string> instructions, List<string> categories)
     {
+        Title = title;
+        Ingredients = new(ingredients);
+        Instructions = new(instructions);
+        Categories = new(categories);
     }
 }
-
 
