0ea38c4fbb6f5b6dd59dac978fc37ca462c58d07
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-08-01 10:58:15 +0200

    add swagger

--- RecipeManagerWithRazorPages/BackEnd/Program.cs
+++ RecipeManagerWithRazorPages/BackEnd/Program.cs
@@ -1,8 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var recipes = new RecipesServices();
 var categories = new CategoriesServices();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-app.MapGet("/", () => "Hello World");
+app.UseSwagger();
+app.UseSwaggerUI();
 categories.Routing(app);
 recipes.Routing(app);
 app.Run();
